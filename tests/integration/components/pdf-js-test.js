bf295b71865eabf2876dd27e771b3ef08cc4f10c
Andreas Andreou <andreoua@gmail.com>
2017-11-06 01:07:43 +0200

    Add example and real test for pdf-js component

--- tests/integration/components/pdf-js-test.js
+++ tests/integration/components/pdf-js-test.js
@@ -1,18 +1,31 @@
+import Ember from 'ember'
 import { moduleForComponent, test } from 'ember-qunit'
 import hbs from 'htmlbars-inline-precompile'
+
+const { run } = Ember
 
 moduleForComponent('pdf-js', 'Integration | Component | pdf js', {
   integration: true
 })
 
 test('it renders', function (assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.expect(4)
+  let done = assert.async()
 
-  this.render(hbs`{{pdf-js}}`)
+  // assertions for then the pdf has been loaded and shown
+  this.set('documentChanged', () => {
+    run.next(() => {
+      assert.equal(this.$('.toolbar label:last').text(), 'page 1 of 14', 'Has 14 pages')
 
-  // check toolbar included
-  assert.equal(this.$('.toolbar').length, 1)
-  // check next/prev buttons exist
-  assert.equal(this.$('.toolbar button').length, 2)
+      this.$('.toolbar button:first').click()
+      assert.equal(this.$('.toolbar label:last').text(), 'page 2 of 14', 'Is on second page')
+
+      done()
+    })
+  })
+
+  this.render(hbs`{{pdf-js pdf="/compressed.tracemonkey-pldi-09.pdf" documentChanged=(action documentChanged)}}`)
+
+  assert.equal(this.$('.toolbar').length, 1, 'Toolbar included')
+  assert.equal(this.$('.toolbar button').length, 2, 'Next/Prev buttons exist')
 })
